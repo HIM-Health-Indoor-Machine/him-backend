2815ef2344051ee8d4d9edd11919edbf9d1a9549
bykang <by28287@gmail.com>
2024-10-21 17:18:26 +0900

    first commit

---
+++ src/main/java/com/him/fpjt/him_backend/HimBackendApplication.java
@@ -0,0 +1,13 @@
+package com.him.fpjt.him_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HimBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HimBackendApplication.class, args);
+	}
+
+}
