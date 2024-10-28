fa042d980be775672986bbbab264209e29c28b85
BOYOUNG-KANG <by28287@gmail.com>
2024-10-28 19:54:58 +0900

    feat : mapperscan 코드 추가

--- src/main/java/com/him/fpjt/him_backend/HimBackendApplication.java
+++ src/main/java/com/him/fpjt/him_backend/HimBackendApplication.java
@@ -1,9 +1,11 @@
 package com.him.fpjt.him_backend;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan(basePackages = "com.him.fpjt.him_backend.exercise.dao")
 public class HimBackendApplication {
 
 	public static void main(String[] args) {
