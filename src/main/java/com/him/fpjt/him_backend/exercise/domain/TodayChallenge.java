d55d6d29919c61951e545dddde116c53b9f85086
BOYOUNG-KANG <by28287@gmail.com>
2024-10-29 15:58:23 +0900

    feat : todayChallenge 생성자 추가

--- src/main/java/com/him/fpjt/him_backend/exercise/domain/TodayChallenge.java
+++ src/main/java/com/him/fpjt/him_backend/exercise/domain/TodayChallenge.java
@@ -1,10 +1,12 @@
 package com.him.fpjt.him_backend.exercise.domain;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.ToString;
 
 @ToString
 @Getter
+@AllArgsConstructor
 public class TodayChallenge {
     private long id;
     private long cnt;
