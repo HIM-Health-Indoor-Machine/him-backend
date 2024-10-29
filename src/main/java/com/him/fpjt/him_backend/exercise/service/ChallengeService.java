8392a78e1b3f6d801723fa28a81e62deffd04225
BOYOUNG-KANG <by28287@gmail.com>
2024-10-29 22:03:47 +0900

    feat: userId, status 별 챌린지 조회 service, controller 기능 추가

--- src/main/java/com/him/fpjt/him_backend/exercise/service/ChallengeService.java
+++ src/main/java/com/him/fpjt/him_backend/exercise/service/ChallengeService.java
@@ -1,9 +1,12 @@
 package com.him.fpjt.him_backend.exercise.service;
 
 import com.him.fpjt.him_backend.exercise.domain.Challenge;
+import com.him.fpjt.him_backend.exercise.domain.ChallengeStatus;
+import java.util.List;
 
 public interface ChallengeService {
     public boolean createChallenge(Challenge challenge);
+    public List<Challenge> getChallengeByStatusAndUserId(long userId, ChallengeStatus status);
     public Challenge getChallengeDetail(long id);
     public boolean removeChallenge(long id);
 }
--- src/main/java/com/him/fpjt/him_backend/exercise/service/ChallengeServiceImpl.java
+++ src/main/java/com/him/fpjt/him_backend/exercise/service/ChallengeServiceImpl.java
@@ -2,6 +2,10 @@ package com.him.fpjt.him_backend.exercise.service;
 
 import com.him.fpjt.him_backend.exercise.dao.ChallengeDao;
 import com.him.fpjt.him_backend.exercise.domain.Challenge;
+import com.him.fpjt.him_backend.exercise.domain.ChallengeStatus;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -18,6 +22,15 @@ public class ChallengeServiceImpl implements ChallengeService {
     public boolean createChallenge(Challenge challenge) {
         return challengeDao.insertChallenge(challenge) > 0 ? true : false;
     }
+
+    @Override
+    public List<Challenge> getChallengeByStatusAndUserId(long userId, ChallengeStatus status) {
+        Map<String, Object> params = new HashMap<>();
+        params.put("userId", userId);
+        params.put("status", ChallengeStatus.ONGOING.name());
+        return challengeDao.selectChallengesByStatusAndUserId(params);
+    }
+
     @Override
     public Challenge getChallengeDetail(long id) {
         return challengeDao.selectChallenge(id);
