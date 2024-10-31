a743fa017079b7862dda535f21a6b71f64f79285
BOYOUNG-KANG <by28287@gmail.com>
2024-10-31 19:20:29 +0900

    feat : 오늘의 챌린지 생성 mapper 추가

---
+++ src/main/java/com/him/fpjt/him_backend/exercise/dao/TodayChallengeDao.java
@@ -0,0 +1,8 @@
+package com.him.fpjt.him_backend.exercise.dao;
+
+import com.him.fpjt.him_backend.exercise.domain.TodayChallenge;
+import java.util.Map;
+
+public interface TodayChallengeDao {
+    public long insertTodayChallenge(TodayChallenge todayChallenge);
+}
