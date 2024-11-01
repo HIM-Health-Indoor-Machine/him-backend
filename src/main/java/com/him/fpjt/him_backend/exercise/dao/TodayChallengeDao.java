19e3cbf2587ecac80fff1421f0180159fdb04047
BOYOUNG-KANG <by28287@gmail.com>
2024-11-01 13:31:21 +0900

    refactor : 필요없는 import 문 삭제

--- src/main/java/com/him/fpjt/him_backend/exercise/dao/TodayChallengeDao.java
+++ src/main/java/com/him/fpjt/him_backend/exercise/dao/TodayChallengeDao.java
@@ -2,7 +2,6 @@ package com.him.fpjt.him_backend.exercise.dao;
 
 import com.him.fpjt.him_backend.exercise.domain.TodayChallenge;
 import java.time.LocalDate;
-import java.util.Map;
 
 public interface TodayChallengeDao {
     public long insertTodayChallenge(TodayChallenge todayChallenge);
