460580ab94be7054ec553c425e3b3631a9023ade
BOYOUNG-KANG <by28287@gmail.com>
2024-11-03 03:57:38 +0900

    feat : 스케줄러 어노테이션 추가

--- src/main/java/com/him/fpjt/him_backend/HimBackendApplication.java
+++ src/main/java/com/him/fpjt/him_backend/HimBackendApplication.java
@@ -4,9 +4,10 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.mybatis.spring.annotation.MapperScans;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@MapperScan(basePackages = "com.him.fpjt.him_backend.exercise.dao")
+@EnableScheduling
 @MapperScans({
 		@MapperScan("com.him.fpjt.him_backend.exercise.dao"),
 		@MapperScan("com.him.fpjt.him_backend.user.dao")
