3fa020f5d85054ef11131ac0c88ac10d50775404
BOYOUNG-KANG <by28287@gmail.com>
2024-11-03 04:26:08 +0900

    feat : 목표 미달성시 경험치 3exp 차감 테스트 코드 추가

--- src/test/java/com/him/fpjt/him_backend/exercise/service/TodayChallengeServiceImplTest.java
+++ src/test/java/com/him/fpjt/him_backend/exercise/service/TodayChallengeServiceImplTest.java
@@ -3,12 +3,26 @@ package com.him.fpjt.him_backend.exercise.service;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import com.him.fpjt.him_backend.common.constants.ExpPoints;
 import com.him.fpjt.him_backend.exercise.dao.TodayChallengeDao;
+import com.him.fpjt.him_backend.exercise.domain.Challenge;
+import com.him.fpjt.him_backend.exercise.domain.ChallengeStatus;
+import com.him.fpjt.him_backend.exercise.domain.ExerciseType;
 import com.him.fpjt.him_backend.exercise.domain.TodayChallenge;
+import com.him.fpjt.him_backend.exercise.dto.TodayChallengeDto;
+import com.him.fpjt.him_backend.user.service.UserService;
 import java.time.LocalDate;
+import java.util.List;
 import java.util.NoSuchElementException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -22,6 +36,8 @@ public class TodayChallengeServiceImplTest {
     private TodayChallengeDao todayChallengeDao;
     @Mock
     private ChallengeService challengeService;
+    @Mock
+    private UserService userService;
     @InjectMocks
     private TodayChallengeServiceImpl todayChallengeService;
     @BeforeEach
@@ -49,7 +65,6 @@ public class TodayChallengeServiceImplTest {
         TodayChallenge todayChallenge = new TodayChallenge(0, 1L, LocalDate.now());
         when(challengeService.existsChallengeById(1L)).thenReturn(false);
 
-        // when & then: 예외 검증
         assertThrows(IllegalArgumentException.class,
                 () -> todayChallengeService.createTodayChallenge(todayChallenge),
                 "존재하지 않는 챌린지 id 입니다."
@@ -96,3 +111,17 @@ public class TodayChallengeServiceImplTest {
         assertThrows(NoSuchElementException.class, () -> todayChallengeService.getTodayChallengeById(1L));
     }
 }
+    @Test
+    @DisplayName("오늘의 챌린지 목표를 달성하지 못했을 시, 경험치 3EXP를 차감한다.")
+    void modifyUnachievementTodayChallenge() {
+        LocalDate yesterday = LocalDate.now().minusDays(1);
+        Challenge challenge = new Challenge(1L, ChallengeStatus.ONGOING, ExerciseType.SQUAT, LocalDate.now(), LocalDate.now(), 10L, 0,1L);
+        TodayChallenge unachievedTodayChallenge = new TodayChallenge(1L, 5L, 1L, yesterday);
+
+        when(todayChallengeDao.findUnachievedChallenges(yesterday)).thenReturn(List.of(unachievedTodayChallenge));
+        when(challengeService.getChallengeDetail(unachievedTodayChallenge.getChallengeId())).thenReturn(challenge);
+        when(userService.modifyUserExp(anyLong(), eq(ExpPoints.DAILY_PENALTY_EXP))).thenReturn(true);
+
+        todayChallengeService.modifyUnachievementTodayChallenge();
+        verify(userService).modifyUserExp(challenge.getUserId(), ExpPoints.DAILY_PENALTY_EXP);
+    }
