5a7e86925a1eebd3978d00f60fba4ddd6770851d
BOYOUNG-KANG <by28287@gmail.com>
2024-11-04 21:49:20 +0900

    feat : userId와 날짜 기간에 따른 출석 리스트 조회 mapper

--- src/main/java/com/him/fpjt/him_backend/user/dao/AttendanceDao.java
+++ src/main/java/com/him/fpjt/him_backend/user/dao/AttendanceDao.java
@@ -2,8 +2,10 @@ package com.him.fpjt.him_backend.user.dao;
 
 import com.him.fpjt.him_backend.user.domain.Attendance;
 import java.time.LocalDate;
+import java.util.List;
 
 public interface AttendanceDao {
     int insertAttendance(Attendance attendance);
     int updateAttendanceStatus(long userId, LocalDate attendDt);
+    List<Attendance> selectAttendanceByUserIdAndDateRange(long userId, LocalDate startOfMonth, LocalDate endOfMonth);
 }
