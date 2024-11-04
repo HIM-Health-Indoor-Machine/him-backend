25185d547595ac078a71cd9a7310fc19952da18b
BOYOUNG-KANG <by28287@gmail.com>
2024-11-04 21:44:27 +0900

    feat : 출석 상태 업데이트 mapper 추가

--- src/main/java/com/him/fpjt/him_backend/user/dao/AttendanceDao.java
+++ src/main/java/com/him/fpjt/him_backend/user/dao/AttendanceDao.java
@@ -1,7 +1,9 @@
 package com.him.fpjt.him_backend.user.dao;
 
 import com.him.fpjt.him_backend.user.domain.Attendance;
+import java.time.LocalDate;
 
 public interface AttendanceDao {
     int insertAttendance(Attendance attendance);
+    int updateAttendanceStatus(long userId, LocalDate attendDt);
 }
