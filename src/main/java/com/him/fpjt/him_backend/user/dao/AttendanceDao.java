cb8bd590c2bc04e66075a1546e3a4f92c9e2e570
BOYOUNG-KANG <by28287@gmail.com>
2024-11-04 21:25:34 +0900

    feat : 오늘 날짜의 attendance 칼럼 생성 mapper 추가

---
+++ src/main/java/com/him/fpjt/him_backend/user/dao/AttendanceDao.java
@@ -0,0 +1,7 @@
+package com.him.fpjt.him_backend.user.dao;
+
+import com.him.fpjt.him_backend.user.domain.Attendance;
+
+public interface AttendanceDao {
+    int insertAttendance(Attendance attendance);
+}
