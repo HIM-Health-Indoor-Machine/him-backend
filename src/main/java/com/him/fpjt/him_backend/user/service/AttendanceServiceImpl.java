e5735eed5354d4151ca24b785c3fde6c36ae1ca1
BOYOUNG-KANG <by28287@gmail.com>
2024-11-04 21:31:29 +0900

    feat : 오늘 날짜 attendance 칼럼 생성 스케줄러 추가
    자정을 기점으로 모든 사용자를 조회하여 오늘 날짜의 출석 칼럼을 생성합니다.

---
+++ src/main/java/com/him/fpjt/him_backend/user/service/AttendanceServiceImpl.java
@@ -0,0 +1,33 @@
+package com.him.fpjt.him_backend.user.service;
+
+import com.him.fpjt.him_backend.common.constants.ExpPoints;
+import com.him.fpjt.him_backend.user.dao.AttendanceDao;
+import com.him.fpjt.him_backend.user.domain.Attendance;
+import java.time.LocalDate;
+import java.time.YearMonth;
+import java.util.List;
+import java.util.NoSuchElementException;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class AttendanceServiceImpl implements AttendenceService{
+    private AttendanceDao attendanceDao;
+    private UserService userService;
+
+    public AttendanceServiceImpl(AttendanceDao attendanceDao, UserService userService) {
+        this.attendanceDao = attendanceDao;
+        this.userService = userService;
+    }
+
+    @Override
+    @Transactional
+    @Scheduled(cron = "0 0 0 * * ?")
+    public void generateDailyAttendance() {
+        List<Long> userIds = userService.getAllUserIds();
+        for (Long userId : userIds) {
+            attendanceDao.insertAttendance(new Attendance(userId));
+        }
+    }
+}
---
+++ src/main/java/com/him/fpjt/him_backend/user/service/AttendenceService.java
@@ -0,0 +1,9 @@
+package com.him.fpjt.him_backend.user.service;
+
+import com.him.fpjt.him_backend.user.domain.Attendance;
+import java.time.LocalDate;
+import java.util.List;
+
+public interface AttendenceService {
+    void generateDailyAttendance();
+}
