daa5fae4f48869ea254cfccdf701e4b3891ffa9a
BOYOUNG-KANG <by28287@gmail.com>
2024-11-08 00:10:21 +0900

    feat : 사용자 정보 조회 mapper 추가

--- src/main/java/com/him/fpjt/him_backend/user/dao/UserDao.java
+++ src/main/java/com/him/fpjt/him_backend/user/dao/UserDao.java
@@ -1,9 +1,11 @@
 package com.him.fpjt.him_backend.user.dao;
 
+import com.him.fpjt.him_backend.user.domain.User;
 import java.util.List;
 
 public interface UserDao {
 
     int updateUserExp(long id, long expPoints);
     List<Long> selectAllUserIds();
+    User selectUserById(long id);
 }
