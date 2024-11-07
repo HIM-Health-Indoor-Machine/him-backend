d0eae9f5826479cb3cb65148b672d632d473c8e0
BOYOUNG-KANG <by28287@gmail.com>
2024-11-08 08:58:01 +0900

    feat : 사용자 정보 수정 테스트 코드 추가

--- src/test/java/com/him/fpjt/him_backend/user/service/UserServiceImplTest.java
+++ src/test/java/com/him/fpjt/him_backend/user/service/UserServiceImplTest.java
@@ -100,4 +100,45 @@ class UserServiceImplTest {
 
         assertThrows(NoSuchElementException.class, () -> userService.getUserById(userId));
     }
+
+    @Test
+    @DisplayName("사용자 정보를 전달할 경우, 사용자의 정보를 수정한다.")
+    void modifyUserInfo_success() {
+        long userId = 1L;
+        User mockUser = new User(userId, "ollie", "email@example.com", "password","profileImg.jpg", Tier.IRON, 2000);
+        UserModifyDto userModifyDto = new UserModifyDto("icarus", "icarus.jpg");
+
+        when(userDao.selectUserById(userId)).thenReturn(mockUser);
+        when(userDao.updateUserInfo(mockUser)).thenReturn(1);
+
+        userService.modifyUserInfo(userId, userModifyDto);
+
+        assertEquals("icarus", mockUser.getNickname());
+        assertEquals("icarus.jpg", mockUser.getProfileImg());
+        verify(userDao).updateUserInfo(mockUser);
+    }
+
+    @Test
+    @DisplayName("사용자 정보 수정에 실패할 경우, IllegalStateException이 발생한다.")
+    void modifyUserInfo_illegalStateException() {
+        long userId = 1L;
+        User mockUser = new User(userId, "ollie", "email@example.com", "password","profileImg.jpg", Tier.IRON, 2000);
+        UserModifyDto userModifyDto = new UserModifyDto("newNickname", "newProfileImg.jpg");
+
+        when(userDao.selectUserById(userId)).thenReturn(mockUser);
+        when(userDao.updateUserInfo(mockUser)).thenReturn(0);
+
+        assertThrows(IllegalStateException.class, () -> userService.modifyUserInfo(userId, userModifyDto));
+    }
+
+    @Test
+    @DisplayName("사용자가 존재하지 않는 경우 NoSuchElementException가 발생한다.")
+    void modifyUserInfo_noSuchElementException() {
+        long userId = 1L;
+        UserModifyDto userModifyDto = new UserModifyDto("newNickname", "newProfileImg.jpg");
+
+        when(userDao.selectUserById(userId)).thenReturn(null);
+
+        assertThrows(NoSuchElementException.class, () -> userService.modifyUserInfo(userId, userModifyDto));
+    }
 }
