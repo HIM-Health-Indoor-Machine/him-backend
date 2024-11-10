b9c33bda38b11ea18db6aa92b8c806e8c8462008
BOYOUNG-KANG <by28287@gmail.com>
2024-11-10 16:57:49 +0900

    refactor : user 로직에 에러 공통 처리 적용

--- src/main/java/com/him/fpjt/him_backend/user/controller/UserController.java
+++ src/main/java/com/him/fpjt/him_backend/user/controller/UserController.java
@@ -3,8 +3,6 @@ package com.him.fpjt.him_backend.user.controller;
 import com.him.fpjt.him_backend.user.dto.UserInfoDto;
 import com.him.fpjt.him_backend.user.dto.UserModifyDto;
 import com.him.fpjt.him_backend.user.service.UserService;
-import java.util.NoSuchElementException;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -24,23 +22,13 @@ public class UserController {
 
     @GetMapping("/{userId}")
     public ResponseEntity<?> getUserInfo(@PathVariable("userId") long userId) {
-        try {
-            UserInfoDto userInfoDto = userService.getUserById(userId);
-            return ResponseEntity.ok().body(userInfoDto);
-        } catch (NoSuchElementException e) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
-        }
+        UserInfoDto userInfoDto = userService.getUserById(userId);
+        return ResponseEntity.ok().body(userInfoDto);
     }
     @PutMapping("/{userId}")
     public ResponseEntity<?> modifyUserInfo(@PathVariable("userId") long userId,
                                     @RequestBody UserModifyDto userModifyDto) {
-        try {
-            userService.modifyUserInfo(userId, userModifyDto);
-            return ResponseEntity.ok().build();
-        } catch (NoSuchElementException e) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
-        } catch (IllegalStateException e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
-        }
+        userService.modifyUserInfo(userId, userModifyDto);
+        return ResponseEntity.ok().build();
     }
 }
