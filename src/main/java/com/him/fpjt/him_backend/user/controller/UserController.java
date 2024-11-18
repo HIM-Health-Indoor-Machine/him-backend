2aa7beaf12338d6bfae2d30917bb0ba0d4d0d540
BOYOUNG-KANG <by28287@gmail.com>
2024-11-18 15:43:21 +0900

    feat: user CORS 설정 추가

--- src/main/java/com/him/fpjt/him_backend/user/controller/UserController.java
+++ src/main/java/com/him/fpjt/him_backend/user/controller/UserController.java
@@ -4,6 +4,7 @@ import com.him.fpjt.him_backend.user.dto.UserInfoDto;
 import com.him.fpjt.him_backend.user.dto.UserModifyDto;
 import com.him.fpjt.him_backend.user.service.UserService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/user")
+@CrossOrigin(origins = "http://localhost:5173")
 public class UserController {
     private UserService userService;
 
